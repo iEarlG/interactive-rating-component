2231aa251666a312e514cfa7a4c3c0d08d2bba3b
Earl Villapaz <jasonburning16@gmail.com>
2023-02-27 19:29:05 +0800

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Ratings from './components/Ratings';
 
 const App = () => {
   return (
     <div>
-      
+      <Ratings />
       <div class="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
         Coded by <a href="https://github.com/iEarlG">Earl A. Villapaz</a>.
---
+++ src/components/Ratings.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Ratings = () => {
+  return (
+    <div>HELLO WORLD!!</div>
+  )
+}
+
+export default Ratings;
